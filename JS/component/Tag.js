8c39818eb234c024135279b263ffbe5bb9b8043e
Ghost <zhan940842903@163.com>
2023-07-11 02:05:31 +0800

    v1.0.2:delete iTagClassStr

--- JS/component/Tag.js
+++ JS/component/Tag.js
@@ -2,19 +2,11 @@ var Tag={
     props: ['tag'],
     template: `
     <span class="badge" v-bind:style="{backgroundColor:'#'+tag.color+' !important',marginRight: '6px'}">
-    <i :class="'fa-brands fa-'+iTagClassStr(tag.name.toLowerCase())+tag.description"></i>
+    <i :class="'fa-brands fa-'+tag.name.toLowerCase()+tag.description"></i>
     {{tag.name}}
     </span>
     `,
     methods:{
-        iTagClassStr(str){
-            switch (str) {
-                case "javascript":
-                    return "js"
-                default:
-                    return str
-            }
-        },
     }
 }
     
