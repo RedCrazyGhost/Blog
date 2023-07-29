753b7dda2ebb160780c86756ab2606e79fe58788
Ghost <zhan940842903@163.com>
2023-07-30 01:23:43 +0800

    v1.0.3:change style

--- JS/component/MiniMarkdown.js
+++ JS/component/MiniMarkdown.js
@@ -13,24 +13,27 @@ var MiniMarkdown={
         this.parserMiniMD()
     },
     template: `
-    <div :class="'card card-'+getAppColor()+' '+getAppClassColor()" style="margin-top: 1.5rem;    padding-right: 0px;
-    padding-left: 0px;">
-        <router-link  :class="getAppClassColor()" :to="{ name: 'watch', params: { targetId: Markdown.number }}">
-        <div :class="'card-header header-'+getAppColor()">    
-        <h5 >{{Markdown.title}} #{{Markdown.number}}</h5>
-        </div>   
-        <div class="card-body">
-            <div :id="'miniMD-'+Markdown.number"></div>
-            <p class="card-text"><small class="text-muted">详细内容请单击查看！</small></p>
-            <p class="card-text">
-            <small class="text-muted"><i class="fa-regular fa-calendar-plus"></i> {{toTime(Markdown.created_at)}}</small> 
-            <small class="text-muted"><i class="fa-solid fa-hourglass-half"></i> {{toTime(Markdown.updated_at)}}</small>
-            </p>
+    <div :class="'offset-1 col-10 card card-'+getAppColor()+' '+getAppClassColor()" style="margin-top: 1.5rem;">
+            <div :class="'card-header header-'+getAppColor()">    
+                 <router-link  :class="getAppClassColor()" :to="{ name: 'watch', params: { targetId: Markdown.number }}">
+                <h5>{{Markdown.title}} #{{Markdown.number}}</h5>
+                 </router-link>
+            </div>   
+            <div class="card-body">
+                <router-link  :class="getAppClassColor()" :to="{ name: 'watch', params: { targetId: Markdown.number }}">
+                <div :id="'miniMD-'+Markdown.number"></div>
+                <p class="card-text"><small class="text-muted">详细内容请单击查看！</small></p>
+                <p class="card-text">
+                <small class="text-muted"><i class="fa-regular fa-calendar-plus"></i> {{toTime(Markdown.created_at)}}</small> 
+                <small class="text-muted"><i class="fa-solid fa-hourglass-half"></i> {{toTime(Markdown.updated_at)}}</small>
+                </p>
+                 </router-link>
             </div>
-            </router-link>
-            <div :class="'card-footer footer-'+getAppColor()" v-if="Markdown.labels.length!==0">
+
+        <div :class="'card-footer footer-'+getAppColor()" v-if="Markdown.labels.length!==0">
             <i-tag  v-for="tag in Markdown.labels" :key="tag.id" :tag="tag"></i-tag>
         </div>
+
     </div>
  
     
