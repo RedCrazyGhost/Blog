f583b9c907ca230d0abb4ef21e3ac7e672497bb1
Ghost <zhan940842903@163.com>
2024-03-09 17:50:02 +0800

    fixed: mermaid render problem

--- src/views/MarkdownView.vue
+++ src/views/MarkdownView.vue
@@ -7,16 +7,21 @@
 import Markdown from '@/components/Markdown.vue';
 import { useRoute } from 'vue-router';
 import {useGithubStore} from '@/stores/Github';
+import { onMounted, onUpdated } from 'vue';
+import mermaid from 'mermaid';
 
+
+onMounted(()=>{
+    mermaid.run();
+})
+
+onUpdated(() => {
+    mermaid.run();
+})
 
 const github = useGithubStore()
 const r = useRoute()
 
 github.GetIssue(Number(r.params.id))
-
-
-
-
-
 
 </script>
