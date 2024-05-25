2390358de7df9ebeeec472e9ad2a657c81eabef0
Ghost <zhan940842903@163.com>
2024-05-25 15:11:20 +0800

    fix: icon load

--- src/components/MarkdownTag.vue
+++ src/components/MarkdownTag.vue
@@ -1,10 +1,10 @@
 <template>
    <span class="badge tag tag-life"  v-if="props.Tag.name === '生活'">
-    <font-awesome-icon class="iconTheme" :icon="'fa-solid fa-'+props.Tag.description"/>
+    <font-awesome-icon class="iconTheme" :icon="props.Tag.description"/>
     {{props.Tag.name}}
     </span>
     <span v-else class="badge tag" v-bind:style="{backgroundColor:'#'+props.Tag.color+' !important'}">
-    <font-awesome-icon class="iconTheme" :icon="'fa-brands fa-'+props.Tag.name.toLowerCase()+props.Tag.description"/>
+    <font-awesome-icon class="iconTheme" :icon="props.Tag.description"/>
     {{props.Tag.name}}
     </span>
 </template>
