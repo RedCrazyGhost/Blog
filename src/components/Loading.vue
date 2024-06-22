4844f0f1f2d71b8724fe7956542b2b6a8e4a91f8
Ghost <zhan940842903@163.com>
2024-06-23 01:20:33 +0800

    fix: change loading

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,14 +1,9 @@
 <template>
-  <div>
     <div
       class="container d-flex align-items-center justify-content-center"
-      style="min-height: calc(100vh - 9.75rem)"
     >
       <font-awesome-icon :icon="['fas', 'rotate']" size="8x" spin />
     </div>
-    <LogosWall />
-  </div>
 </template>
 <script setup lang="ts">
-import LogosWall from "@/components/LogosWall.vue";
 </script>
