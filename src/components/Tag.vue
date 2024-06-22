a8b10997b31545a4f8d80e8a0ef5d142a0a6b721
Ghost <zhan940842903@163.com>
2024-06-22 18:23:19 +0800

    feat: add tag style

--- src/components/Tag.vue
+++ src/components/Tag.vue
@@ -1,26 +1,31 @@
 <template>
-   <span class="badge tag tag-life"  v-if="props.Tag.name === '生活'">
-    <font-awesome-icon class="iconTheme" :icon="props.Tag.description"/>
-    {{props.Tag.name}}
-    </span>
-    <span v-else class="badge tag" v-bind:style="{backgroundColor:'#'+props.Tag.color+' !important'}">
-    <font-awesome-icon class="iconTheme" :icon="props.Tag.description"/>
+    <span :class="'tag badge '+tagStyle" :style="{backgroundColor: '#'+props.Tag.color}">
+    <font-awesome-icon class="iconTheme" :icon="iconStyle"/>
     {{props.Tag.name}}
     </span>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue'
 const props = defineProps(["Tag"])
+const iconStyle = ref<string[]>([])
+const tagStyle = ref<string[]>([])
 
-// todo props.Tag.description 支持更多自定义配置
-// 拆分成数组判断本文前缀，逐一添加选项
-// ["fa-tag", "fa-tags", "css-life", "css-fly"]
+props.Tag.description.split(" ").forEach((element : string) => {
+    if (element.startsWith("fa-")) {
+        iconStyle.value.push(element)
+    }
+    if (element.startsWith("tag-")) {
+        tagStyle.value.push(element)
+    }
+});
+
 
 </script>
 <style>
 .tag{
     margin-right: 6px;
 }
-.tag-life{
-    background: linear-gradient( 217deg, rgba(255, 0, 0, 0.8), rgba(255, 0, 0, 0) 70.71% ), linear-gradient(127deg, rgba(0, 255, 0, 0.8), rgba(0, 255, 0, 0) 70.71%), linear-gradient(336deg, rgba(0, 0, 255, 0.8), rgba(0, 0, 255, 0) 70.71%);
+.tag-rainbow{
+    background: linear-gradient( 217deg, rgba(255, 0, 0, 0.8), rgba(255, 0, 0, 0) 70.71% ), linear-gradient(127deg, rgba(0, 255, 0, 0.8), rgba(0, 255, 0, 0) 70.71%), linear-gradient(336deg, rgba(0, 0, 255, 0.8), rgba(0, 0, 255, 0) 70.71%) !important;
 }
 </style>
